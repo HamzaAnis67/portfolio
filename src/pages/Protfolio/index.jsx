a239efaa18a16d4f74d11810b0bfa928ad0f6694
darkdevil0012 <smarthamza123@outlook.com>
2025-07-22 01:15:15 +0500

    portfolio section updated

--- src/pages/Protfolio/index.jsx
+++ src/pages/Protfolio/index.jsx
@@ -5,57 +5,14 @@ import Col from "react-bootstrap/Col";
 import "./portfolio.css";
 import { useState } from "react";
 export const Protfolio = () => {
-	const [hover, setHover] = useState("nodisplay");
-	const [hover2, setHover2] = useState("nodisplay");
-	const HoverData = "My First Web";
-
-	const onHover = (e) => {
-		e.preventDefault();
-		setHover("displayed");
-		console.log("hovered");
-	};
-
-	const onHoverOver = (e) => {
-		e.preventDefault();
-		setHover("nodisplay");
-	};
-	const onHover2 = (e) => {
-		e.preventDefault();
-		setHover2("displayed2");
-		console.log("hovered");
-	};
-	const onHoverOver2 = (e) => {
-		e.preventDefault();
-		setHover2("nodisplay");
-	};
-
 	return (
 		<Container>
 			<div className="main_heading">
 				<h1>My Portfolio</h1>
-				{/* <div className="sub_headings" id="pic1">
-					<a href="#pic1">All</a>
-				</div> */}
 
 				<div className="portfolioimages">
-					<Row>
+					<Row className="g-5">
 						<Col sx={12} sm={6} md={4}>
-							{/* <div className="firstimg">
-								<a
-									href="https://e-com-frontend-roan.vercel.app/"
-									target="blank">
-									<img
-										src="https://res.cloudinary.com/dnqplq5hb/image/upload/v1726320593/euvqteypqzvmm8tryolo.png"
-										alt=""
-										className="image1 zoom"
-										onMouseEnter={(e) => onHover(e)}
-										onMouseLeave={(e) => onHoverOver(e)}
-									/>
-								</a>
-								<div>
-									<h3 className={hover}>{HoverData}</h3>
-								</div>
-							</div> */}
 							<Card style={{ width: "18rem" }}>
 								<Card.Img
 									variant="top"
@@ -70,22 +27,6 @@ export const Protfolio = () => {
 							</Card>
 						</Col>
 						<Col sx={12} sm={6} md={4}>
-							{/* <div className="firstimg portfolio_imgleft">
-								<a
-									href="https://e-com-frontend-roan.vercel.app/"
-									target="blank">
-									<img
-										src="http://res.cloudinary.com/dnqplq5hb/image/upload/v1726320733/bub2zzuomzjbmc78ge0x.jpg"
-										alt=""
-										className="image1 zoom"
-										onMouseEnter={(e) => onHover2(e)}
-										onMouseLeave={(e) => onHoverOver2(e)}
-									/>
-								</a>
-								<div className="imgtext">
-									<h3 className={hover2}>{HoverData}</h3>
-								</div>
-							</div> */}
 							<Card style={{ width: "18rem" }}>
 								<Card.Img
 									variant="top"
@@ -97,6 +38,38 @@ export const Protfolio = () => {
 										href="https://portfolio-six-nu-85.vercel.app/"
 										style={{ textDecoration: "none", color: "black" }}>
 										<Card.Title>Portfolio Website</Card.Title>
+									</a>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col sx={12} sm={6} md={4}>
+							<Card style={{ width: "18rem" }}>
+								<Card.Img
+									variant="top"
+									src="http://res.cloudinary.com/dnqplq5hb/image/upload/v1753113649/coactbpokkdt3omdpv6i.png"
+									height={"375rem"}
+								/>
+								<Card.Body>
+									<a
+										href="https://homepage-design-delta.vercel.app/index.html"
+										style={{ textDecoration: "none", color: "black" }}>
+										<Card.Title>Internship Project</Card.Title>
+									</a>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col sx={12} sm={6} md={4}>
+							<Card style={{ width: "18rem" }}>
+								<Card.Img
+									variant="top"
+									src="http://res.cloudinary.com/dnqplq5hb/image/upload/v1753128496/acwseomzhddlrr3nyuez.png"
+									height={"375rem"}
+								/>
+								<Card.Body>
+									<a
+										href="https://portfolio-six-nu-85.vercel.app/"
+										style={{ textDecoration: "none", color: "black" }}>
+										<Card.Title>University Project</Card.Title>
 									</a>
 								</Card.Body>
 							</Card>
